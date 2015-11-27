bf08427469f8a682ee282b18d219e4ee540d3c7b
Scott Bellware <sbellware@gmail.com>
2015-11-27 14:05:09 -0600

    Subject can be object or class

--- test/spec/formatted_data.rb
+++ test/spec/formatted_data.rb
@@ -5,7 +5,7 @@ describe "Get Formatted Data from Serializer Before it's Converted" do
     example = Serialize::Controls.example
     raw_data = Serialize::Write.raw_data(example, :some_serializer)
 
-    formatted_data = Serialize::Write.formatted_data(raw_data, example.class, :some_serializer)
+    formatted_data = Serialize::Write.formatted_data(raw_data, example, :some_serializer)
 
     assert(formatted_data == 'some formatted data')
   end
